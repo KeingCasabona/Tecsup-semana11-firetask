5260d09774e9cfa622cc716bc409d06698e39252
KeingCasabona <keing.limache.casabona@gmail.com>
2024-10-23 18:04:49 -0500

    feat: Add Configurando proyecto en IOS (no se pudo en Android)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firetask/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
